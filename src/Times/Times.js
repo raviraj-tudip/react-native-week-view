d565e611b784977abf24e4a20f3a933c6769bbb3
raviraj-tudip <58065231+raviraj-tudip@users.noreply.github.com>
2022-06-04 01:13:45 +0530

    Update Times.js

--- src/Times/Times.js
+++ src/Times/Times.js
@@ -9,9 +9,33 @@ const Times = ({ times, hoursInDisplay, timeStep, textStyle, width }) => {
   return (
     <View style={[styles.columnContainer, { width }]}>
       {times.map((time) => (
-        <View key={time} style={[styles.label, { height }]}>
-          <Text style={[styles.text, textStyle]}>{time}</Text>
-        </View>
+      // <View key={time} style={[styles.label, { height }]}>
+      // <Text style={[styles.text, textStyle]}>{time}</Text>
+      // </View>
+
+      /**
+         * Mapped Lables name with time index
+         * So, Lable name can be visible to user
+         */
+        <View key={time} style={[styles.label, { height, justifyContent: 'center', alignItems: 'center', backgroundColor: '#EFEFEF', margin: 2, borderRadius: 4}]}>
+        <Text style={[styles.text, textStyle, {transform: [{ rotate: '270deg' }]}, ]}>{
+        time === '0:00' 
+        ?
+        'Weed Control'
+        :
+        time === '1:00'
+        ?
+        'Disease Control'
+        :
+        time === '2:00'
+        ?
+        'Insection Control'
+        :
+        time === '3:00'
+        &&
+        'Bio Solution'
+      }</Text>
+      </View>
       ))}
     </View>
   );
